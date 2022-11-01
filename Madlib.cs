714622d4cc3378228cce10a5c3de239f27ab1399
kingar7 <kingar7@ceclnx01-19.csi.miamioh.edu>
2022-11-01 13:40:39 -0400

    Submitting

--- Madlib.cs
+++ Madlib.cs
@@ -1,4 +1,18 @@
 // TODO:  Write the Madlib program in C#
 //
+using System; 
+
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Enter a Name: Enter a Color: Enter a Noun (pl): ");
+            string name = Console.ReadLine();
+            string color = Console.ReadLine();
+            string noun = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine($"Roses are {color}");
+            Console.WriteLine($"{noun} are blue,");
+            Console.WriteLine($"{name} loves you.");
+        }
+    }
 
 
